aeb7d5f874728baa1db89f0b252afb5d0a509ee3
cys <cys@ux303>
2018-05-23 00:10:31 +0800

    init version

---
+++ demo1/src/views/FeiView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      <img src='../assets/fei.png'>
+      <br/>
+      萌萌哒小肥在此
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .hello{
+        margin-top: 40px;
+        text-align: center;
+    }
+</style>
